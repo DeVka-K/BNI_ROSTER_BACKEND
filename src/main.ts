d6ac329c46fa2323cdf3af3b199f2a213715c9da
Nuthaim9744 <nuthaim.shukoorphasesintern@outlook.com>
2024-07-08 14:00:17 +0530

    csv to pdf conversion

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,35 @@
 // }
 // bootstrap();
 
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.useGlobalPipes(new ValidationPipe());
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+ 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  // Custom CORS configuration
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000', // Allow only this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
+ 
+  await app.listen(4000);
 }
-bootstrap();
+bootstrap()
